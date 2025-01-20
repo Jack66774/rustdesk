ff17ccf3c3dbd5ed73d257638eb0206e9053fce6
Jack66774 <36321113@qq.com>
2025-01-20 20:35:37 +0800

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -48,10 +48,10 @@ fn main() {
         -k, --key=[KEY] ''
        -s, --server=[] 'Start server'",
     );
-    let matches = App::new("rustdesk")
+    let matches = App::new("华晨教育")
         .version(crate::VERSION)
-        .author("Purslane Ltd<info@rustdesk.com>")
-        .about("RustDesk command line tool")
+        .author("华晨教育<36321113@qq.com>")
+        .about("华晨教育 command line tool")
         .args_from_usage(&args)
         .get_matches();
     use hbb_common::{config::LocalConfig, env_logger::*};
